c42e72e39b8e30694b42d28c3c9754e784e9d46e
AlexShashkov <sh@dmitrov-dubna.ru>
2022-08-23 14:14:29 +0300

    Minor changes, new app with arguments

--- App.py
+++ App.py
@@ -1,21 +1,12 @@
-from consolemenu import *
-from consolemenu.items import *
-from numpy import longcomplex as lc
-
+import argparse
 from Deg3Solvers.Baydoun import Solver as BSolver
 from Deg3Solvers.Vieta import Solver as VSolver
-
 from MethodsArray import Array
-
 from Generation.methods import *
 from Generation.GenerateEquations import *
 
-
-menu = ConsoleMenu("PolynomDeg3Solver", "КМБО-03-20, 2022")
-methods = {
-    "Vieta": VSolver(),
-    "Baydoun": BSolver()
-}
+from consolemenu import *
+from consolemenu.items import *
 
 def inp(method):
     print("Введите через пробел коэффициенты при c, x, x^2, x^3\n")
@@ -29,19 +20,47 @@ def inp(method):
         print(f"Коэффициенты введены неверно. {ex}")
         input()
 
-Vieta_item = FunctionItem("Виета", inp, ["Vieta"])
-Baydoun_item = FunctionItem("Baydoun", inp, ["Baydoun"])
-Testing_item2 = FunctionItem("Тестирование с комплексными корнями",
-        StartEquationsTest, args=[generateComplexEquations], kwargs=methods)
-Testing_item3 = FunctionItem("Тестирование с малыми комплексными корнями",
-        StartEquationsMinValueTest, args=[generateIntegerEquations], kwargs=methods)
-Testing_item4 = FunctionItem("Тестирование с малыми комплексными корнями (Exp)",
-        StartEquationsMinExpValueTest, args=[generateExponentComplexEquations], kwargs=methods)
+methods = {
+    "Vieta": VSolver(),
+    "Baydoun": BSolver()
+}
 
-menu.append_item(Vieta_item)
-menu.append_item(Baydoun_item)
-menu.append_item(Testing_item2)
-menu.append_item(Testing_item3)
-# menu.append_item(Testing_item4)
+tests = {
+    "Complex": None,
+    "ComplexSmall": None
+}
 
-menu.show()
+parser = argparse.ArgumentParser(description="App for solving and testing \
+    Vieta/Baydoun methods ")
+parser.add_argument("-m", "--method", help="Launches Vieta OR Baydoun method",\
+        default=None)
+parser.add_argument("-t", "--test", \
+        help="Run test:1 for random complex numbers, 2 for small random \
+        complex numbers", default=None)
+parser.add_argument("-si", "--saveinput", help="Saves initial input",\
+        action="store_true")
+parser.add_argument("-s", "--saveresult", help="Saves results", action="store_true")
+parser.add_argument("-p", "--plot", help="Plot test results", action="store_true")
+
+args = parser.parse_args()
+print(args)
+
+if args.method is None and args.test is None:
+    menu = ConsoleMenu("PolynomDeg3Solver", "КМБО-03-20, 2022")
+    Vieta_item = FunctionItem("Виета", inp, ["Vieta"])
+    Baydoun_item = FunctionItem("Baydoun", inp, ["Baydoun"])
+    Testing_item2 = FunctionItem("Тестирование с комплексными корнями",
+            StartEquationsTest, args=[generateComplexEquations], kwargs=methods)
+    Testing_item3 = FunctionItem("Тестирование с малыми комплексными корнями",
+            StartEquationsMinValueTest, args=[generateIntegerEquations], kwargs=methods)
+    Testing_item4 = FunctionItem("Тестирование с малыми комплексными корнями (Exp)",
+            StartEquationsMinExpValueTest, args=[generateExponentComplexEquations], kwargs=methods)
+    menu.append_item(Vieta_item)
+    menu.append_item(Baydoun_item)
+    menu.append_item(Testing_item2)
+    menu.append_item(Testing_item3)
+    # menu.append_item(Testing_item4)
+    menu.show()
+elif args.method in methods.keys():
+    inp(args.method)
+elif args.test in tests.keys(): print("Amog us")
